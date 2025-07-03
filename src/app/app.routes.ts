e35ec154754470fbf61512b41bf67529eb6fa4b6
layesow <abdoulayesow716@gmail.com>
2025-07-03 13:12:52 +0000

    creation de la page login et routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { LayoutComponent } from './pages/layout/layout.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { EmployeeComponent } from './pages/employee/employee.component';
 
 export const routes: Routes = [
   {
@@ -17,7 +19,18 @@ export const routes: Routes = [
   {
     //layout
     path: '',
-    component: LayoutComponent
+    component: LayoutComponent,
+    children: [
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      },
+      {
+        // employee management
+        path: 'employees',
+        component: EmployeeComponent
+      }
+    ]
   }
 
 ];
