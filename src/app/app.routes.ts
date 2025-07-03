40b1b79d4fe58b30e0d051b7278078e7fcdad092
layesow <abdoulayesow716@gmail.com>
2025-07-03 11:47:37 +0000

    creation de composants

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { LayoutComponent } from './pages/layout/layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    //login page
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    //login page
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    //layout
+    path: '',
+    component: LayoutComponent
+  }
+
+];
---
+++ src/app/pages/project-emplyees/project-emplyees.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-project-emplyees',
+  imports: [],
+  templateUrl: './project-emplyees.component.html',
+  styleUrl: './project-emplyees.component.css'
+})
+export class ProjectEmplyeesComponent {
+
+}
